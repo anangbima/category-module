ef1170a72473695145507afc936907480b25a0e9
Anang <anangbima11@gmail.com>
2024-12-07 12:17:29 +0700

    push

---
+++ src/Models/Category.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Packages\Razen\Category_module\Src\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name'
+    ];
+}
